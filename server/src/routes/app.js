e870dc3b333655e89ecc245d9c70da8bb893abcd
Batait <velichko.dmitrii97@gmail.com>
2024-05-02 10:22:22 +0300

    test

--- server/src/routes/app.js
+++ server/src/routes/app.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
-
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(express.json());
 
